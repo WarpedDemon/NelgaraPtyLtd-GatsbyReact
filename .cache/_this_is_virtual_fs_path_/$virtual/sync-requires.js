b5f10601325afc4b090d8bcf0d27441d4cbfd488
James Noonan <warped.demon.prince@hotmail.com>
2021-07-29 22:17:00 +0800

    Update

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\warpe\\Desktop\\EndProject\\NelgaraPtyLtd-GatsbyReact\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Users\\warpe\\Desktop\\EndProject\\NelgaraPtyLtd-GatsbyReact\\src\\pages\\404.js")),
   "component---src-pages-contactus-js": preferDefault(require("C:\\Users\\warpe\\Desktop\\EndProject\\NelgaraPtyLtd-GatsbyReact\\src\\pages\\contactus.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\warpe\\Desktop\\EndProject\\NelgaraPtyLtd-GatsbyReact\\src\\pages\\index.js")),
