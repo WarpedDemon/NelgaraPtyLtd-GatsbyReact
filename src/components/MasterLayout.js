a7254f7651be36c158d3ff82e6349da4076345b5
EC2 Default User <ec2-user@ip-172-31-17-54.ap-southeast-2.compute.internal>
2022-07-11 13:25:29 +0000

    Hopefully final colour fixing.

--- src/components/MasterLayout.js
+++ src/components/MasterLayout.js
@@ -4,7 +4,7 @@ import Footer from "./Footer"
 import Banner from "./Banner"
 import {Helmet} from "react-helmet";
 
-
+import "../styles/theme.css"
 import "../styles/LandingPage.css"
 import "../styles/Mobile.css"
 import "../styles/Extras.css"
@@ -13,7 +13,17 @@ import "../styles/animate.css"
 
 
 export default function MasterLayout({ children, title }) {
-
+    
+    function AssessTheme() {
+        if (typeof window !== 'undefined') {
+            var storedTheme = localStorage.getItem('theme');
+            if (storedTheme) {
+                document.documentElement.setAttribute('data-theme', storedTheme);
+            }
+        }
+    }
+    
+    AssessTheme();
     return (
 
         <div className="MasterLayout">
